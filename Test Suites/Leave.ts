ac66d43e6adf90c0e5821c3795a114c66e74fd3f
tameemazeem <tameem@myquickhr.com>
2025-01-02 14:36:32 +0800

    Test case

---
+++ Test Suites/Leave.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Leave</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>74f5e269-d3c6-4101-b9e7-fe526c3cc94e</testSuiteGuid>
+</TestSuiteEntity>
